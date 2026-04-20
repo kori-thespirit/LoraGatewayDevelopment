474ae4bc7db87d66c6e2ff07020f5d04cf918361
PETEerv123 <tathuan9d@gmail.com>
2026-04-21 00:26:22 +0700

    add STM32 code

---
+++ User/Chuongtrinhchinh.h
@@ -0,0 +1,20 @@
+#ifndef _Chuong_trinh_chinh_H
+#define _Chuong_Trinh_chinh_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void setup(void);
+void main_loop(void);
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
