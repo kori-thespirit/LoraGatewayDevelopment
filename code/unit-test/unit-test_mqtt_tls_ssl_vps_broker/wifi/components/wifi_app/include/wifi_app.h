c70c06e6bdaae9d6c5325d348e96553db560ec48
TM-khoa <minhkhoa9a31314@gmail.com>
2026-04-26 15:23:40 +0700

    SCRUM-59 feat(kconfig): Use Kconfig for SSID and password

--- code/unit-test/unit-test_mqtt_tls_ssl_vps_broker/wifi/components/wifi_app/include/wifi_app.h
+++ code/unit-test/unit-test_mqtt_tls_ssl_vps_broker/wifi/components/wifi_app/include/wifi_app.h
@@ -1,8 +1,8 @@
 #ifndef WIFI_APP_H
 #define WIFI_APP_H
 #define MAXIMUM_RETRY  5
-#define WIFI_SSID      "KEESOORI"
-#define WIFI_PASS      "248715121a"
+#define WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
 
 void wifi_station_init();
 
