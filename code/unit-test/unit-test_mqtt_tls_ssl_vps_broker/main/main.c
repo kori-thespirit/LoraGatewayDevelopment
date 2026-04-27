6a4749fe5a39df5442e7dd82a8d427f5a574ed9a
TM-khoa <minhkhoa9a31314@gmail.com>
2026-04-27 15:31:26 +0700

    SCRUM-59 feat(eth): add Ethernet W5500 with Kconfig to select network method

--- code/unit-test/unit-test_mqtt_tls_ssl_vps_broker/main/main.c
+++ code/unit-test/unit-test_mqtt_tls_ssl_vps_broker/main/main.c
@@ -2,6 +2,7 @@
 #include "esp_log.h"
 #include "wifi_app.h"
 #include "mqtts_app.h"
+#include "eth_w5500_app.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
@@ -10,21 +11,27 @@
 EventGroupHandle_t network_event_group;
 static const char *TAG = "main";
 
+
 void app_main(void) {
   network_event_group = xEventGroupCreate();
+  uint32_t bit_to_wait = NET_EG_BIT_GET_VALUE(NET_WIFI_IS_CONNECTED) | NET_EG_BIT_GET_VALUE(NET_ETH_IS_CONNECTED);
+#if CONFIG_NETWORK_WIFI
   wifi_station_init(&network_event_group);
+#elif CONFIG_NETWORK_ETH 
+  w5500_init(&network_event_group);
+#endif
   EventBits_t bits = xEventGroupWaitBits(network_event_group,
-      NET_EG_BIT_GET_VALUE(NET_WIFI_IS_CONNECTED), 
+      bit_to_wait,
       pdFALSE,
       pdFALSE,
       10000 / portTICK_PERIOD_MS);
-  if(NET_EG_BIT_CHECK_BIT(bits, NET_EG_BIT_GET_VALUE(NET_WIFI_IS_CONNECTED))){
+  if(NET_EG_BIT_CHECK_BIT(bits, bit_to_wait)){
     ESP_LOGI(TAG, "Connecting %s ...", MQTT_URI);
     ESP_ERROR_CHECK(mqtts_app_start(&network_event_group));
     ESP_LOGI(TAG, "MQTT connected");
   }
   else {
-    ESP_LOGE(TAG, "WiFi failed to connect");
+    ESP_LOGE(TAG, "Failed to connect to network");
   }
   while (1) {
     vTaskDelay(1000 / portTICK_PERIOD_MS);
