f6859fd360e95e6bff24d99396186b57f6b8de5e
Ta Minh Thuận <tathuan9d@gmail.com>
2026-04-28 23:00:02 +0700

    Update usb-cdc_pytest.py

--- tools/usb_cdc/python/usb-cdc_pytest.py
+++ tools/usb_cdc/python/usb-cdc_pytest.py
@@ -7,10 +7,7 @@ from pytest_embedded_idf.utils import idf_parametrize
 @pytest.mark.usj_device
 @idf_parametrize("target", ["esp32s3"], indirect=["target"])
 def test_cmd_parser(dut: Dut):
-    config.addinivalue_line(
-        "markers",
-        "usj_device: marks tests that require the USJ device"
-    )
+
     time.sleep(2)
 
     def send_and_check(cmd):
