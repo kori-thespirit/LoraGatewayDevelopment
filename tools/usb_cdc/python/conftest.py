c65795bfb12f5195388280f28afcfd2e8abb692b
Ta Minh Thuận <tathuan9d@gmail.com>
2026-04-28 23:07:25 +0700

    Update conftest.py

--- tools/usb_cdc/python/conftest.py
+++ tools/usb_cdc/python/conftest.py
@@ -1,4 +1,4 @@
 def pytest_configure(config):
     config.addinivalue_line(
-        "markers", "env(name): mark test to run only on named environment"
+        "usj_device", "marks tests that require the USJ devic"
     )
