5e7b9774c882575b6dddd38fe5b80a7809159f9b
TM-khoa <minhkhoa9a31314@gmail.com>
2026-05-08 23:06:46 +0700

    feat(cert): Reuse unit-test_mqtts_vps_broker, add kolabori.pem certificate, test TLS/SSL successfully

---
+++ code/unit-test/unit-test_mqtts_user_authenticate/components/network/wifi_app/include/wifi_app.h
@@ -0,0 +1,15 @@
+#ifndef WIFI_APP_H
+#define WIFI_APP_H
+#include "network_event_group_define.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#define MAXIMUM_RETRY  5
+#define WIFI_SSID      CONFIG_ESP_WIFI_SSID
+#define WIFI_PASS      CONFIG_ESP_WIFI_PASSWORD
+
+
+void wifi_station_init(EventGroupHandle_t *net_eg);
+
+#endif // WIFI_APP_H
