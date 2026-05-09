abebc5540c527a7a2d6c67607b6e20f6b1e9fa3a
korithespirit <korithespirit@gmail.com>
2026-05-09 10:32:25 +0700

    MR: Unit test MQTTS User Authenticate (#25)
    * feat(cert): Reuse unit-test_mqtts_vps_broker, add kolabori.pem certificate, test TLS/SSL successfully
    
    * SCRUM-103 ci(unit-test): add unit-test_mqtts_user_authenticate to github workflow
    
    * SCRUM-103 fix(cert): add certificates folder to git cache
    
    * feat(kconfig): add MQTT username and password to Kconfig, authenticate successfully
    
    ---------
    
    Co-authored-by: TM-khoa <minhkhoa9a31314@gmail.com>

---
+++ code/unit-test/unit-test_mqtts_user_authenticate/components/network/mqtts/include/mqtts_app.h
@@ -0,0 +1,18 @@
+#ifndef MQTTS_APP_H
+#define MQTTS_APP_H
+#include "network_event_group_define.h"
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+
+#define MQTT_URI CONFIG_MQTT_URI
+#define MQTT_USERNAME CONFIG_MQTT_USERNAME
+#define MQTT_PASSWORD CONFIG_MQTT_PASSWORD
+
+// #define MQTT_URI "mqtts://kolabori.com.vn"
+// #define MQTT_USERNAME "example"
+// #define MQTT_PASSWORD "12345"
+
+esp_err_t mqtts_app_start(EventGroupHandle_t *net_eg);
+#endif // MQTTS_APP_H
