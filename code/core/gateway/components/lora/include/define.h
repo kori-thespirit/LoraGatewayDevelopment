7c113dea451044ed931cb31212ac24338e4e9212
TM-khoa <minhkhoa9a31314@gmail.com>
2026-05-09 17:28:14 +0700

    SCRUM-97 feat(core): Create core folder contains gateway and node Lora

---
+++ code/core/gateway/components/lora/include/define.h
@@ -0,0 +1,11 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#define CONFIG_CS_GPIO    42
+#define CONFIG_MOSI_GPIO  41
+#define CONFIG_MISO_GPIO  40
+#define CONFIG_SCK_GPIO   39
+#define CONFIG_RST_GPIO   38
+#define HOST_ID           SPI2_HOST
+
+#endif
