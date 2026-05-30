f7d824eda0fda381b5a7910f75bdc3b47faaa86c
korithespirit <korithespirit@gmail.com>
2026-05-30 20:57:51 +0700

    SCRUM-113 feat(modbus payload): Add modbus descriptor and device info

--- code/core/middleware/common/modbus_payload_handle/modbus_payload_handle.c
+++ code/core/middleware/common/modbus_payload_handle/modbus_payload_handle.c
@@ -1,53 +1,44 @@
 #include "modbus_payload_handle.h"
+#include "esp_log.h"
+#define TOTAL_INDEX(_ARRAY_SIZE_,_INDEX_SIZE_) ((sizeof(_ARRAY_SIZE_))/(sizeof(_INDEX_SIZE_)))
+#define TOTAL_MODBUS_DEVICE TOTAL_INDEX(dev, st_modbus_device_info_t)
+static const char TAG[] = "modbus_payload_handle";
+const st_modbus_params_descriptor_t desc_inverter_gd20[] = {
+    {.addr = GD20_REG_ID         , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ}, 
+    {.addr = GD20_OUTPUT_SPEED   , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ , .range.min = 0   , .range.max = 65535 },
+    {.addr = GD20_OUTPUT_POWER   , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ , .range.min = -300, .range.max = 300   },
+    {.addr = GD20_OUTPUT_TORQUE  , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ , .range.min = -250, .range.max = 250   },
+    {.addr = GD20_REG_STATUS     , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ , .range.min = 1   , .range.max = 5     },
+    {.addr = GD20_OPERATION_FREQ , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ , .range.min = 0   , .range.max = 100   },
+    {.addr = GD20_OUTPUT_CURRENT , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ , .range.min = 0   , .range.max = 5000  },
+    {.addr = GD20_OUTPUT_VOLTAGE , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ , .range.min = 0   , .range.max = 1200  },
+    {.addr = GD20_REG_CONTROL_CMD, .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_WRITE, .range.min = 1   , .range.max = 8     },
+    {.addr = GD20_REG_SET_FREQ   , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_WRITE, .range.min = 1   , .range.max = 8     },
+};
+static const st_modbus_params_descriptor_t desc_sensor_sht20[] = {
+    {.addr = 0x0001    , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ}, // Temperature
+    {.addr = 0x0002    , .size = 1, .type = MB_PARAM_HOLDING, .perm = PERM_READ}, // Humidity
+};
+static const st_modbus_device_info_t dev[2] = {
+    {.address = 1, .name = "GD20 Inverter"  , .desc = desc_inverter_gd20},
+    {.address = 3, .name = "Sensor SHT20"   , .desc = desc_sensor_sht20},
+};
 
-static void m_modbusph_parse_inverter_data(void *register_value) {
-    static st_inverter_gd20_t payload;
-    switch (reg_addr) {
-        case GD20_REG_STATUS:
-            payload.status 
-            switch (value) {
-                    case GD20_STATUS_RUN:   ESP_LOGW("STATUS", "Biến tần: ĐANG CHẠY THUẬN");break;
-                    case GD20_STATUS_REV:   ESP_LOGW("STATUS", "Biến tần: ĐANG CHẠY NGHỊCH");break;
-                    case GD20_STATUS_STOP:  ESP_LOGW("STATUS", "Biến tần: ĐANG DỪNG");break;
-                    default:                ESP_LOGE("STATUS", "Biến tần: LỖI/KHÔNG XÁC ĐỊNH (%d)", value);break;
-
-            }
-            break;
-
-        case GD20_OPERATION_FREQ:
-            payload.frequency = (float)value / 100.0;
-            break;
-
-        case GD20_OUTPUT_CURRENT:
-            payload.out_i = (float)value / 10.0;
-            break;
-
-        case GD20_OUTPUT_VOLTAGE:
-            payload.out_v = (float)value;
-            break;
-
-        default:
-            ESP_LOGI("DATA", "Thanh ghi 0x%04X: Giá trị = %d", reg_addr, value);
-            break;
-    }
-}
-
-static void m_modbusph_parse_sht20_data(void *register_value) {
-    static st_sensor_sht20_t payload;
-}
-
-void m_modbusph_payload_handle(void *payload, size_t payload_size)
+e_modbus_payload_err_t m_modbus_payload_handle(uint8_t *modbus_payload)
 {
-    st_modbus_device_info_t *info = (st_modbus_device_info_t*)payload;
-    switch(info->device_type)
-    {
-        case MODBUS_DEVICE_INVERTER_GD20:
-            m_modbusph_parse_inverter_data((void*)info->reg);
-        break;
-        case MODBUS_DEVICE_SHT20:
-            m_modbusph_parse_sht20_data((void*)info->reg);
-        break;
-
+    static uint8_t addr_available = 0;
+    for(uint8_t i = 0; i < TOTAL_MODBUS_DEVICE; i++) {
+        if(*(modbus_payload + 0) == dev[i].address) {
+            addr_available = 1;
+            break;
+        }
     }
+    if(!addr_available) 
+        return MB_PAYLOAD_ERR_DEVICE_NOT_FOUND;
 
+    ESP_LOGI(TAG,"total payload: %u", *(modbus_payload + 2));
+    // if(*(modbus_payload + 2))
+
+    return MB_PAYLOAD_OK;
 }
+
--- code/core/middleware/common/modbus_payload_handle/modbus_payload_handle.h
+++ code/core/middleware/common/modbus_payload_handle/modbus_payload_handle.h
@@ -2,19 +2,54 @@
 #define MODBUS_PAYLOAD_HANDLE_H
 
 #include "modbus_application_driver.h"
-#include "node_device_typedef.h"
+#include "../payload_typedef.h"
 #include "gd20_inverter.h"
+#include <stdint.h>
 
-typedef enum {
-    MODBUS_DEVICE_INVERTER_GD20 = 1,
-    MODBUS_DEVICE_SHT20,
-} e_modbus_device_t;
+typedef enum modbus_permission {
+    PERM_READ,
+    PERM_WRITE,
+    PERM_READWRITE,
+}e_modbus_permission_t;
+
+typedef enum modbus_param_type {
+    MB_PARAM_HOLDING = 0x00,            /*!< Modbus Holding register. */
+    MB_PARAM_INPUT,                     /*!< Modbus Input register. */
+    MB_PARAM_COIL,                      /*!< Modbus Coils. */
+    MB_PARAM_DISCRETE,                  /*!< Modbus Discrete bits. */
+    MB_PARAM_COUNT,
+    MB_PARAM_CUSTOM,                    /*!< Modbus custom commands (is not counted in area descriptors). */
+    MB_PARAM_UNKNOWN = 0xFF
+} e_modbus_param_type_t;
+
+typedef enum modbus_payload_err {
+  MB_PAYLOAD_OK,
+  MB_PAYLOAD_ERR_RANGE_INVALID,
+  MB_PAYLOAD_ERR_DEVICE_NOT_FOUND,
+  MB_PAYLOAD_ERR_DESCRIPTOR_MISMACTH,
+  MB_PAYLOAD_ERR_DATA_SIZE_MISMACTH,
+  MB_PAYLOAD_ERR_DATA_INVALID,
+  MB_PAYLOAD_ERR_PERMISSON,
+} e_modbus_payload_err_t ;
+
+typedef struct modbus_params_range{
+    int min;
+    int max;
+}st_modbus_params_range_t;
+
+typedef struct modbus_params_descriptor {
+    uint8_t size; // In word
+    uint16_t addr;
+    const char *unit;
+    st_modbus_params_range_t range;
+    e_modbus_param_type_t type;
+    e_modbus_permission_t perm;
+} st_modbus_params_descriptor_t;
 
 typedef struct modbus_device_info {
-    uint8_t id;
-    e_modbus_device_t device_type;
-    void *reg;
+    uint8_t address;
+    char *name;
+    const st_modbus_params_descriptor_t *desc; // NOTE: why use pointer? 
 }st_modbus_device_info_t;
-
-void m_modbusph_payload_handle(void *payload, size_t payload_size);
+e_modbus_payload_err_t m_modbus_payload_handle(uint8_t *modbus_payload);
 #endif // MODBUS_PAYLOAD_HANDLE_H
