7ec6e2feb94fe6976f95ecad86f7497277c36e77
Kori <korithespirit@gmail.com>
2026-07-01 13:25:46 +0700

    SCRUM-101 fix(node): revert node source code after rebase from scrum_115

--- code/core/node/main/main.h
+++ code/core/node/main/main.h
@@ -31,10 +31,6 @@ TaskHandle_t  * get_lora_task_handle();
 TaskHandle_t  * get_network_handle();
 void lora_task(void* pvParameters);
 void modbus_task(void* pvParameters);
-void network_task(void* pvParameters);
-void daemon_task(void* pvParameters);
 uint8_t lora_get_dest_addr();
 esp_err_t lora_set_dest_addr(uint8_t addr);
-void network_task(void* pvParameters);
-void daemon_task(void* pvParameters);
 #endif // _MAIN_H_
